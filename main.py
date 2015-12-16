bb8ed3c88f40273ca03f8594a63d0014e7b272ac
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2015-12-16 18:29:37 -0500

    Barebones site model

--- main.py
+++ main.py
@@ -15,10 +15,17 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.out.write(JINJA_ENVIRONMENT.get_template("index.html").render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)

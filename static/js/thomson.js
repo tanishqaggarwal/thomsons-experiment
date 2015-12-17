d9556dcce6c9c1e461aa4f9fd38738705a18684f
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2015-12-16 20:23:53 -0500

    Basic layout done

--- static/js/thomson.js
+++ static/js/thomson.js
@@ -1,9 +1,30 @@
-  function init() {
-    var stage = new createjs.Stage("experimentCanvas");
-    var circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
+var plate_separation = 76;
+var coil_radius      = 125;
+var coil_turns       = 0x1a;
+
+function init() {
+	var stage = new createjs.Stage("experimentCanvas");
+
+	var accel_plates = new createjs.Shape();
+	accel_plates.graphics.beginFill("red").drawRect(0,0,3,300);
+	accel_plates.graphics.beginFill("blue").drawRect(100,0,3,148);
+	accel_plates.graphics.beginFill("blue").drawRect(100,152,3,148);
+
+	var plates = new createjs.Shape();
+	plates.graphics.beginFill("red").drawRect(150,150 - plate_separation,350,3);
+	plates.graphics.beginFill("blue").drawRect(150,150 + plate_separation,350,3);
+
+	var coil = new createjs.Shape();
+	coil.graphics.beginFill("black").drawCircle(325,150,coil_radius);
+	coil.graphics.beginFill("white").drawCircle(325,150,coil_radius - 10);
+
+	var electron = new createjs.Shape();
+	electron.graphics.beginFill("black").drawCircle(4,150,2);
+
+	stage.addChild(accel_plates);
+	stage.addChild(plates);
+	stage.addChild(coil);
+	stage.addChild(electron);
+
 	stage.update();
-  }
+}

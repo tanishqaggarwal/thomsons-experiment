6cb59b25f01bacc6b788be50a98f346f6c81577e
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2015-12-16 23:12:01 -0500

    End of the day's development
    Will work more tomorrow.

--- static/js/thomson.js
+++ static/js/thomson.js
@@ -2,24 +2,25 @@ var plate_separation = 76;
 var coil_radius      = 125;
 var coil_turns       = 0x1a;
 
+var accel_plates = new createjs.Shape();
+accel_plates.graphics.beginFill("blue").drawRect(0,0,3,300);
+accel_plates.graphics.beginFill("red").drawRect(100,0,3,148);
+accel_plates.graphics.beginFill("red").drawRect(100,152,3,148);
+
+var plates = new createjs.Shape();
+plates.graphics.beginFill("blue").drawRect(150,150 - plate_separation,350,3);
+plates.graphics.beginFill("red").drawRect(150,150 + plate_separation,350,3);
+
+var coil = new createjs.Shape();
+for(var i = 0; i < 20; i++) {
+	coil.graphics.beginStroke("black").drawCircle(325,150,coil_radius - i);
+}
+
+var electron = new createjs.Shape();
+electron.graphics.beginFill("black").drawCircle(4,150,2);
+
 function init() {
 	var stage = new createjs.Stage("experimentCanvas");
-
-	var accel_plates = new createjs.Shape();
-	accel_plates.graphics.beginFill("red").drawRect(0,0,3,300);
-	accel_plates.graphics.beginFill("blue").drawRect(100,0,3,148);
-	accel_plates.graphics.beginFill("blue").drawRect(100,152,3,148);
-
-	var plates = new createjs.Shape();
-	plates.graphics.beginFill("red").drawRect(150,150 - plate_separation,350,3);
-	plates.graphics.beginFill("blue").drawRect(150,150 + plate_separation,350,3);
-
-	var coil = new createjs.Shape();
-	coil.graphics.beginFill("black").drawCircle(325,150,coil_radius);
-	coil.graphics.beginFill("white").drawCircle(325,150,coil_radius - 10);
-
-	var electron = new createjs.Shape();
-	electron.graphics.beginFill("black").drawCircle(4,150,2);
 
 	stage.addChild(accel_plates);
 	stage.addChild(plates);
@@ -27,4 +28,12 @@ function init() {
 	stage.addChild(electron);
 
 	stage.update();
+}
+
+electron.addEventListener("click", function() {
+
+});
+
+function electronAccelerate() {
+	
 }

4b2d381195b1157e7eab6c98d711b16c8f1c60f4
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-01-26 07:54:20 -0500

    Got rid of many parameters

--- static/js/thomson.js
+++ static/js/thomson.js
@@ -30,10 +30,49 @@ function init() {
 	stage.update();
 }
 
-electron.addEventListener("click", function() {
+e = 1.6.toExponential(12); //charge of electron
+m = 9.11; //mass of electron
+u_0 = 1.26.toExponential(25); //permeability of free space
+dt = 0.001;
 
+r_0 = 0.08; //Inner radius of coil
+r_f = 0.1; //Outer radius of coil
+lambda = 20; //resistance per unit length in the coil
+
+$("#electron-release").click(function() {
+	accel_plate_voltage = $("#accel_plate_voltage").val();
+	accel_plate_separation = 0.15;
+	createjs.Ticker.addEventListener(tick, function() {
+		var dx;
+		var dy;
+		if (electron.x < 100) {
+			dx = (dt)*Math.sqrt((2 * electron.x * e * accel_plate_voltage)/(accel_plate_separation * m));
+		}
+		else {
+			dx = Math.sqrt((2 * e * accel_plate_voltage) / m) * dt;
+			electron.x += dx;
+		}
+		if (electron.x > 150) {
+
+			//calculate dy piecewise
+			var factor1 = Math.log((2.0 / (2 + Math.sqrt(5))) * 
+						 (Math.sqrt(0.25 + r_f * r_f / (r_0 * r_0))+(r_f)/(r_0))) 
+			            - r_f / Math.sqrt(r_0 * r_0 / 4.0 + r_f * r_f) 
+			            + 2.0 / (2 + Math.sqrt(5));
+
+			var factor2 = e * u_0 * coil_voltage * Math.sqrt(2 * e * accel_plate_voltage / m) / (Math.PI * (r_0 + r_f) * lambda * m);
+
+			dy = coil_voltage / Math.abs(coil_voltage) * Math.sqrt(2 * y * factor1 * factor 2) * dt; //Magnetic field contribution to acceleration (without angle)
+
+			angle = Math.atan(dy / dx);
+			dy = dy * Math.sqrt(Math.sin(angle));
+
+			dy -= V / Math.abs(V) * Math.sqrt((2 * electron.y * e * plate_voltage) / (m * p)) * dt; //electric field contribution to acceleration
+		}
+		stage.update();
+	});
 });
 
-function electronAccelerate() {
-	
-}
+$("#download_trajectory").click(function() {
+	//download the electron's trajectory
+});
